ea5abc58df92853f346bc40cd9c08738b09d4e8c
varunu28 <varun.u28@gmail.com>
2018-11-14 18:46:16 -0800

    Successfully pushing data to DynamoDB from S3 bucket

--- script.py
+++ script.py
@@ -6,19 +6,25 @@ def lambda_handler(event, context):
     
     if event:
         print("Event: ", event)
-
         file_obj = event["Records"][0]
         filename = str(file_obj['s3']['object']['key'])
-
         print("Filename: ", filename)
-
         fileObj = s3.get_object(Bucket='test-bucket-cmpe295', Key=filename)
         file_content = fileObj["Body"].read().decode("utf-8")
-
         print(file_content)
         
         file_json = json.loads(file_content)
-        print(file_json["text"])
+        print(str(file_json["user"]["location"]))
         
-    return "Hello World"
+        dynamo_db = boto3.resource('dynamodb')
+        dynamoTable = dynamo_db.Table('cmpe295A')
+        
+        dynamoTable.put_item(Item = {
+            'id': file_json['id_str'],
+            'tweet_date': file_json['created_at'],
+            'tweet_text': file_json['text'],
+            'tweet_location': str(file_json["user"]["location"])
+        })
+        
+    return "Testing Lambda Function"
 
